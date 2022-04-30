14dc7b9b60937a4b4bdce88051697a8f63e49569
Myles J <hi@mylesj.dev>
2022-04-30 23:00:04 +0100

    feat: mvp delivered - semantic release (#14)

--- src/main.tsx
+++ src/main.tsx
@@ -7,11 +7,16 @@ import { THEME_NAME } from '~/constants'
 import html from './html'
 import View from './view'
 
+import pkg from '../package.json'
+
 export const render: Renderer = async (resume) => {
     const locale = await getLocale(resume.meta?.[THEME_NAME]?.locale)
 
     return html({
-        meta: resume.basics,
+        resume,
+        meta: {
+            generator: `${pkg.name}@${pkg.version}`,
+        },
         body: renderToString(
             <AppContext.Provider value={{ resume, locale }}>
                 <View />
