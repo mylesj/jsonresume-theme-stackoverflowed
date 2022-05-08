969f021d1384578fdb4e7ff7a07cf9c38f1f03fa
Myles J <hi@mylesj.dev>
2022-05-08 18:05:28 +0100

    chore: correct inport typo

--- src/middleware/resume/index.ts
+++ src/middleware/resume/index.ts
@@ -3,7 +3,7 @@ import { compose } from 'immer-compose'
 import { ResumeSchema } from '~/types'
 
 import { profileImage } from './profileImage'
-import { stackOverflow } from './stackoverflow'
+import { stackOverflow } from './stackOverflow'
 
 export const resumeMiddleware = compose<ResumeSchema>(
     profileImage,
